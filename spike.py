fa1c8a0a25db8b568e909c3a1bbf56cfdc0baf03
Rabea Gleissner <rabeagleissner@gmail.com>
2020-02-26 08:31:52 +0800

    Template and post route

--- spike.py
+++ spike.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello'
+    return 'Hello World'
+
+@app.route('/with-template/')
+@app.route('/with-template/<name>')
+def with_template(name=None):
+    return render_template('test_template.html', name=name)
+
+@app.route('/form-data', methods=['POST'])
+def login():
+    print('Request object:')
+    print(request)
+    print(request.form)
+    print(request.form['name'])
+    return 'Thanks!'
+
