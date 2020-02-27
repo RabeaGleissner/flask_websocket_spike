9c9ca2f1b0a8c853c8d023963384f20ee7bfb549
Rabea Gleissner <rabeagleissner@gmail.com>
2020-02-27 10:39:46 +0800

    Install SocketIO for Flask

--- spike.py
+++ spike.py
@@ -1,11 +1,32 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'very-secret'
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
     return 'Hello World'
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    print('socketio.on my_event')
+    emit('my_response', {'data': message['data']})
+
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_message(message):
+    print('socketio.on my_broadcast_event')
+    emit('my_response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    print('socketio.on connect')
+    send('my_response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
 
 @app.route('/with-template/')
 @app.route('/with-template/<name>')
@@ -20,3 +41,5 @@ def login():
     print(request.form['name'])
     return 'Thanks!'
 
+if __name__ == '__main__':
+    socketio.run(app)
